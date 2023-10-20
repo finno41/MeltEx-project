5b426143c54ee32dd4977d6cb25b4ae9f404190a
Oli Finn <74098129+finno41@users.noreply.github.com>
2023-10-20 15:32:14 +0100

    listing search

--- meltexapp/dto/listing.py
+++ meltexapp/dto/listing.py
@@ -1,15 +1,39 @@
 from meltexapp.helper.base_dto import BaseDTOCollection, BaseDTO
 from meltexapp.data.geography import get_geographies_by_ids
+from meltexapp.data.sub_asset_class import get_sub_assets_by_ids
 import pandas as pd
 
 
 class ListingDTO(BaseDTO):
-    def __init__(self, data, user, geog_df=None):
+    def __init__(self, data, user, geog_df=pd.DataFrame(), ac_df=pd.DataFrame()):
         geog_id = data["geography_id"]
+        sub_ac_id = data["sub_asset_class_id"]
+        if geog_df.empty:
+            geographies = get_geographies_by_ids(user, [geog_id]).values()
+            geog_df = pd.DataFrame(geographies).set_index("id")
+            geog_df = pd.DataFrame(geographies).set_index("id")
         geog_info = geog_df.loc[[geog_id]]
         self.geography = geog_info["name"][0]
+        if ac_df.empty:
+            sub_acs = get_sub_assets_by_ids(user, [sub_ac_id]).values(
+                "id", "name", "asset_class__name"
+            )
+            ac_df = pd.DataFrame(sub_acs).set_index("id")
+        ac_info = ac_df.loc[[sub_ac_id]]
+        self.asset_class_name = ac_info["asset_class__name"][0]
+        self.sub_asset_class_name = ac_info["name"][0]
+
         super().__init__(
-            data, user, hide_keys=["id", "geography_id", "owner_id", "public"]
+            data,
+            user,
+            hide_keys=[
+                "id",
+                "geography_id",
+                "owner_id",
+                "public",
+                "asset_class_id",
+                "sub_asset_class_id",
+            ],
         )
 
 
@@ -17,7 +41,17 @@ class ListingDTOCollection(BaseDTOCollection):
     def __init__(self, data, user):
         if not isinstance(data, list):
             data = list(data.values())
-        geography_ids = [d["geography_id"].hex for d in data]
-        geographies = get_geographies_by_ids(user, geography_ids).values()
-        geog_df = pd.DataFrame(geographies).set_index("id")
-        super().__init__(data, user, base_dto=ListingDTO, geog_df=geog_df)
+        if not data:
+            super().__init__(data, user, base_dto=ListingDTO)
+        else:
+            geography_ids = [d["geography_id"].hex for d in data]
+            geographies = get_geographies_by_ids(user, geography_ids).values()
+            geog_df = pd.DataFrame(geographies).set_index("id")
+            sub_ac_ids = [d["sub_asset_class_id"].hex for d in data]
+            sub_acs = get_sub_assets_by_ids(user, sub_ac_ids).values(
+                "id", "name", "asset_class__name"
+            )
+            ac_df = pd.DataFrame(sub_acs).set_index("id")
+            super().__init__(
+                data, user, base_dto=ListingDTO, geog_df=geog_df, ac_df=ac_df
+            )
--- meltexapp/service/listing/search.py
+++ meltexapp/service/listing/search.py
@@ -1,17 +1,29 @@
 from meltexapp.data.listing import filter_listing
 from meltexapp.data.geography import get_geo_children_with_parent
+from meltexapp.data.sub_asset_class import get_sub_acs_by_ac, get_sub_acs_by_acs
 
 
 def listing_search(
-    user, asset_class_name=None, sub_asset_class_name=None, geography_id=None
+    user,
+    asset_class_name=None,
+    sub_asset_class_name=None,
+    geography_id=None,
+    ac_id=None,
 ):
     geos = (
         list(get_geo_children_with_parent(user, geography_id)) if geography_id else None
+    )
+    ac_id = None if ac_id == ["all"] else ac_id
+    sub_asset_class_ids = (
+        list(get_sub_acs_by_acs(user, ac_id).values_list("id", flat=True))
+        if ac_id
+        else None
     )
     filters = {
         "asset_class_name": asset_class_name,
         "sub_asset_class_name": sub_asset_class_name,
         "geography__in": geos,
+        "sub_asset_class__in": sub_asset_class_ids,
     }
     filters = {k: v for k, v in filters.items() if v}
     return filter_listing(user, **filters)
