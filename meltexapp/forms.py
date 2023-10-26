378ac468847fe06534966eb9b20fd7794f15af2d
Oli Finn <74098129+finno41@users.noreply.github.com>
2023-10-26 13:35:37 +0100

    date stays populated update

--- meltexapp/forms.py
+++ meltexapp/forms.py
@@ -27,6 +27,7 @@ class ListingForm(forms.ModelForm):
             "risk_prof",
             "fund_ter",
             "comments",
+            "expr_int_ddline",
         ]
 
     def __init__(self, user, *args, **kwargs):
@@ -34,26 +35,28 @@ class ListingForm(forms.ModelForm):
             user, format="tuple"
         )
         select_fields = ["asset_class", "sub_asset_class", "geography"]
+        date_fields = ["expr_int_ddline"]
         self.declared_fields["asset_class"].choices = AC_CHOICES
-        self.base_fields["expr_int_ddline"] = forms.DateField(
-            widget=forms.DateInput(attrs={"type": "date"})
-        )
+        # self.base_fields["expr_int_ddline"] = forms.DateField(
+        #     widget=forms.DateInput(attrs={"type": "date"})
+        # )
         for field_type, fields in {
             "base_fields": self.base_fields,
             "declared_fields": self.declared_fields,
         }.items():
             for field in fields:
                 if field in select_fields:
-                    getattr(self, field_type)[field].widget.attrs[
-                        "class"
-                    ] = "form-select"
+                    field_attr = getattr(self, field_type)
+                    field_attr[field].widget.attrs["class"] = "form-select"
                 else:
-                    getattr(self, field_type)[field].widget.attrs[
-                        "class"
-                    ] = "form-control"
-                if getattr(self, field_type)[field].required:
-                    getattr(self, field_type)[field].required = False
-                    getattr(self, field_type)[field].widget.attrs["required"] = True
+                    field_attr[field].widget.attrs["class"] = "form-control"
+                if field_attr[field].required:
+                    field_attr[field].required = False
+                    field_attr[field].widget.attrs["required"] = True
+                if field in date_fields:
+                    field_attr[field].widget.attrs["type"] = "date"
+                    field_attr[field].widget.input_type = "date"
+                    field_attr[field].format = "%Y-%m-%d"
 
         self.base_fields["sub_asset_class"].queryset = SubAssetClass.objects.none()
         self.base_fields["geography"].queryset = get_permitted_geographies(
@@ -76,5 +79,6 @@ class ListingForm(forms.ModelForm):
         "targ_irr",
         "risk_prof",
         "fund_ter",
+        "expr_int_ddline",
         "comments",
     ]
