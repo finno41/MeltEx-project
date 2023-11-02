1a9d0d4c1646d11f181c7b8b8080b041fcef2ddd
Oli Finn <74098129+finno41@users.noreply.github.com>
2023-11-02 19:58:31 +0000

    change to docker root

--- meltex/settings.py
+++ meltex/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
 import environ
 
@@ -17,8 +16,6 @@ env = environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
