9cf405860e5f50277c59695c1306e82306814c45
Oli Finn <74098129+finno41@users.noreply.github.com>
2023-12-05 15:45:05 +0000

    display only valid exp_int_ddline

--- meltexapp/service/listing/search.py
+++ meltexapp/service/listing/search.py
@@ -1,6 +1,7 @@
 from meltexapp.data.listing import filter_listing
 from meltexapp.data.geography import get_geo_children_with_parent
 from meltexapp.data.sub_asset_class import get_sub_acs_by_ac, get_sub_acs_by_acs
+from datetime import datetime
 
 
 def listing_search(
@@ -10,6 +11,7 @@ def listing_search(
     geography_id=None,
     ac_id=None,
     company_only=False,
+    valid_exp_int_ddline=True,
 ):
     geos = (
         list(get_geo_children_with_parent(
@@ -28,6 +30,7 @@ def listing_search(
         "sub_asset_class__in": sub_asset_class_ids,
     }
     filters = {k: v for k, v in filters.items() if v}
-    filter_listings = filter_listing(user, company_only, **filters)
+    filter_listings = filter_listing(
+        user, company_only, valid_exp_int_ddline=valid_exp_int_ddline, **filters)
     filter_listings = filter_listings.order_by("expr_int_ddline", "id")
     return filter_listings
