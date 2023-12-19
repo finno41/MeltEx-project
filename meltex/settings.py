8d7826b677722013f22f76d9ca403059e0079687
Oli Finn <74098129+finno41@users.noreply.github.com>
2023-12-19 20:42:15 +0000

    Update settings.py

--- meltex/settings.py
+++ meltex/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-raa_)uczfns2bd8y&-vl5cg0gvyrk29%sy641kwem$809elt2g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = "meltexapp.User"
 # Application definition
