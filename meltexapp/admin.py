c836e7086f5e0f1dad41bcacf43a03571358de95
Oli Finn <74098129+finno41@users.noreply.github.com>
2023-12-22 15:21:04 +0000

    add user fields to admin

--- meltexapp/admin.py
+++ meltexapp/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from meltexapp.models import User
 
+class UserAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
 admin.site.register(User, UserAdmin)
--- meltexapp/models.py
+++ meltexapp/models.py
@@ -10,6 +10,9 @@ class Company(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     type = models.CharField(max_length=50, blank=False, null=False)
 
+    def __str__(self):
+        return self.name
+
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=20, blank=True, null=True)
