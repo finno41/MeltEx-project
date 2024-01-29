5778d955dbbfd99ad02eaf5a760b01a7c7fc1510
Oli Finn <74098129+finno41@users.noreply.github.com>
2024-01-29 15:52:53 +0000

    remove console logs

--- meltexapp/static/javascript/pop-up.js
+++ meltexapp/static/javascript/pop-up.js
@@ -1,7 +1,6 @@
 var popupLink = document.getElementById("popup-link");
 var popupWindow = document.getElementById("popup-window");
 var closeButtons = document.querySelectorAll(".close-button");
-console.log(closeButtons)
 
 popupLink.addEventListener("click", function (event) {
   event.preventDefault();
@@ -9,7 +8,6 @@ popupLink.addEventListener("click", function (event) {
 });
 
 closeButtons.forEach(function (closeButton) {
-  console.log(closeButton)
   closeButton.addEventListener("click", function () {
     popupWindow.style.display = "none";
   });
