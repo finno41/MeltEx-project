021c14c349ee97c9d9ee5bc62fb24819d675670a
Oli Finn <74098129+finno41@users.noreply.github.com>
2024-02-26 17:56:31 +0000

    filter page working

--- meltexapp/service/listing/filter.py
+++ meltexapp/service/listing/filter.py
@@ -1,4 +1,4 @@
-from meltexapp.config.listing import LISTING_CONFIG
+from meltexapp.config.listing import column_ids_names
 from meltexapp.helper.asset_class import get_asset_class_options
 from meltexapp.helper.geography import get_continents_countries
 from meltexapp.helper.general import bulk_manipulate_dict_list
@@ -6,17 +6,15 @@ import pandas as pd
 
 
 def get_filter_options(user):
-    columns = LISTING_CONFIG
+    columns = column_ids_names()
     asset_classes = get_asset_class_options(user)
     asset_class_filter_data = bulk_manipulate_dict_list(
         asset_classes,
-        from_to_map={"id": "key"},
         add_keys={"default": True},
     )
     continents = get_continents_countries(user, continents_only=True)
     continents_filter_data = bulk_manipulate_dict_list(
         continents,
-        from_to_map={"id": "key"},
         add_keys={"default": True},
     )
     return asset_class_filter_data, continents_filter_data, columns
@@ -38,7 +36,7 @@ def add_filter_formatting(asset_class_filter, continents_filter, columns_filter)
         },
         {
             "name": "Asset Class",
-            "key": "asset_class",
+            "key": "ac_id",
             "type": "tickbox",
             "options": asset_class_filter,
         },
