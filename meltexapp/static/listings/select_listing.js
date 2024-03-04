339dea6fd2f8b5dd1a3f5b852dfa433a5585d6a1
Oli Finn <74098129+finno41@users.noreply.github.com>
2024-03-04 18:16:20 +0000

    ensure only one element can be highlighted at a time

--- meltexapp/static/listings/select_listing.js
+++ meltexapp/static/listings/select_listing.js
@@ -4,6 +4,9 @@ console.log(listingCards)
 
 listingCards.forEach(listingCard => {
   listingCard.addEventListener('click', function () {
+    listingCards.forEach(listingCard => {
+      listingCard.classList.remove("menu-listing-card-selected")
+    })
     listingCard.classList.add("menu-listing-card-selected")
   })
 })
