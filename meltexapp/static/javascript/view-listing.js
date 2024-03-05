aab5c6b8c054ac71087b1d4919b8b6b00238e6c8
Oli Finn <74098129+finno41@users.noreply.github.com>
2024-03-05 13:03:24 +0000

    remove unused logic for pop-up

--- meltexapp/static/javascript/view-listing.js
+++ meltexapp/static/javascript/view-listing.js
@@ -58,49 +58,6 @@ function addEventListenerToCancelButton() {
   var cancelButton = document.getElementById("cancel-register-interest-button")
   cancelButton.addEventListener("click", hideRegisterInterestForm)
 }
-function closePopUpWindow() {
-  popupWindow.style.display = "none";
-  popUpContent.innerHTML = "";
-}
-
-closeButtons.forEach(function (closeButton) {
-  closeButton.addEventListener("click", function () {
-    closePopUpWindow()
-  });
-});
-
-
-document.addEventListener('click', function (event) {
-  showListingPopUp = document.getElementById("show-listing-pop-up")
-  if (event.target !== showListingPopUp && !showListingPopUp.contains(event.target)) {
-    closePopUpWindow()
-  }
-});
-
-addPopUpEventListener()
-
-function addPopUpEventListener() {
-  var popupLinks = document.querySelectorAll(".popup-link");
-  popupLinks.forEach(popupLink => {
-    const listingId = popupLink.id;
-    popupLink.addEventListener("click", function (event) {
-      event.preventDefault();
-      const url = `/listings/show_listing/${listingId}`;
-      fetch(url)
-        .then(response => response.text())
-        .then(html => {
-          popUpContent.innerHTML = html;
-          popupWindow.style.display = "block"
-          addEventListenerToRegisterInterest()
-          addEventListenerToCancelButton()
-          addFetchToInterestSubmit(listingId)
-        })
-        .catch(error => {
-          console.error('Error fetching HTML:', error);
-        });
-    })
-  })
-}
 
 function getCookie(name) {
   let cookieValue = null;
